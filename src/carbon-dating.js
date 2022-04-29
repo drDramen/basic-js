54e78158cf3d81a1e0d9dcd53ba8dc3bb4759f6d
drDramen <vvpalchin@gmail.com>
2022-04-30 01:58:40 +0300

    refactor: change approach to input data validation

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,7 +6,7 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
+ *
  * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
@@ -17,19 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
- function dateSample(sampleActivity) {
+function dateSample(sampleActivity) {
   if (
-    typeof sampleActivity === 'string' &&
-    +sampleActivity &&
-    !Number.isNaN(sampleActivity) &&
-    sampleActivity <= MODERN_ACTIVITY &&
-    sampleActivity > 0
-  ) {
-    const k = Math.LN2 / 5730;
-    return Math.ceil(Math.log(15 / sampleActivity) / k);
-  }
+    typeof sampleActivity !== 'string'
+    || sampleActivity > MODERN_ACTIVITY
+    || sampleActivity < 0
+  ) return false;
 
-  return false;
+  const k = Math.LN2 / 5730;
+
+  return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / k);
 }
 
 module.exports = {
