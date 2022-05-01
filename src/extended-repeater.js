34de23bf17cdc3876106a5e472b927da38bfc8c4
drDramen <vvpalchin@gmail.com>
2022-05-01 20:40:19 +0300

    feat: implement Extended repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,22 +2,45 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const {
+    repeatTimes: rT,
+    separator: s,
+    addition: addStr,
+    additionRepeatTimes: aRt,
+    additionSeparator: aS
+  } = options;
+
+  const repeat = (times = 1) => {
+    return (data = '') => Array(times).fill(`${data}`);
+  }
+
+  const join = (separator) => {
+    return (array = []) => array.join(separator);
+  }
+
+  const composition = (...args) => {
+    return (string) => {
+      return args.reduce((init, current) => current(init), string);
+    }
+  }
+
+  const addStrComposition = composition(repeat(aRt), join(aS || '|'))(addStr);
+
+  return composition(repeat(rT), join(s || '+'))(str + addStrComposition);
 }
 
 module.exports = {
